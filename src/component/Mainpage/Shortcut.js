a835cb49ab593d391ce3a6ef2e874784bb71dbcd
TP1997 <thanhphamlike@gmail.com>
2021-10-03 22:49:21 -0400

    Atempted to redesign-not done

--- src/component/Mainpage/Shortcut.js
+++ src/component/Mainpage/Shortcut.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Shortcut = (props) => {
   return (
     <div className={props.class}>
-      <a href={props.href}>
-        <img src={props.src} alt={props.alt}/> 
+      <img src={props.imgSrc} alt={props.imgAlt}/> 
+      <a href={props.aHref}>
         <p>{props.desc}</p>
       </a>
     </div>
