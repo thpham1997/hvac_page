c10c0450209d5ccfe69bd665966cbf93784e143e
TP1997 <thanhphamlike@gmail.com>
2021-10-03 22:47:06 -0400

    Added topics section to page

--- src/component/Mainpage/Mainpage.js
+++ src/component/Mainpage/Mainpage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './_mainpage.scss'
 import Backgroundheader from './BackgroundHeader'
+import Topicshortcut from './TopicShortcut';
 const Mainpage = () => {
   return (
     <div className='mainPage'>
       <Backgroundheader class='mainPage__bgImage' header='THIS IS A TITLE'/>
+      <Topicshortcut class='mainPage__topicShortcut'/>
     </div>
   );
 }
