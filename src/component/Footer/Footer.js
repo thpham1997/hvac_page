60eb88824577995ef166a5b2f98687ad14d0156a
TP1997 <thanhphamlike@gmail.com>
2021-10-06 10:59:37 -0400

    Created basic footer

---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './_footer.scss'
+const Footer = (props) => {
+  return (
+    <div className='footer'>
+      <div className='footer__info1'>Information 1</div>
+      <div className='footer__info2'>Information 2</div>
+      <div className='footer__info3'>Information 3</div>
+    </div>
+  );
+}
+
+export default Footer;
