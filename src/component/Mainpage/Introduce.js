05803a1e5f217ea871cab5210f6a6dda5ba951b8
TP1997 <thanhphamlike@gmail.com>
2021-12-12 20:13:23 -0500

    added intro portion into main page

---
+++ src/component/Mainpage/Introduce.js
@@ -0,0 +1,24 @@
+
+import React from 'react'
+import worker from '../../img/worker.jpeg'
+import { Link } from 'react-router-dom'
+export default function Introduce(props) {
+  return (
+    <div className={`${props.parentName}__intro`}>
+      <div className={`${props.parentName}__introText`}>
+        <h2>This is a slogan: Ipsum culpa magna irure.</h2>
+        <p>This is an introduction1: Lorem esse nulla elit consectetur elit excepteur aliqua pariatur.</p>
+        <p>This is an introduction2: Lorem esse nulla elit consectetur elit excepteur aliqua pariatur.</p>
+        <p>This is an introduction3: Lorem esse nulla elit consectetur elit excepteur aliqua pariatur.</p>
+        <Link to="/">Our Service</Link>
+      </div>
+      <div className={`${props.parentName}__introImg`}>
+        <img src={worker} alt='woker Image'></img>
+        <div className={`${props.parentName}__introCard`}>
+          <h2>Our Mission</h2>
+          <p>Adipisicing pariatur ea eu. Deserunt cupidatat cillum voluptate est pariatur pariatur ex labore laborum eu est tempor do.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
