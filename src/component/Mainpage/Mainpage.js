f02bbebfa120e42ba56b17b40ec8900cb4e7e3e6
TP1997 <thanhphamlike@gmail.com>
2021-12-12 22:43:51 -0500

    Added OurWork section

--- src/component/Mainpage/Mainpage.js
+++ src/component/Mainpage/Mainpage.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './_mainpage.scss'
 import Backgroundheader from './BackgroundHeader'
 import Topicshortcut from './TopicShortcut';
+import Introduce from './Introduce';
+import OurWork from './OurWork';
 const Mainpage = () => {
   return (
     <div className='mainPage'>
       <Backgroundheader class='mainPage__bgImage' header='HVAC Group-Working for The Best'/>
-      <Topicshortcut class='mainPage__topicShortcut'/>
+      {/* <Topicshortcut class='mainPage__topicShortcut'/> */}
+      <Introduce parentName='mainPage'></Introduce>
+      <OurWork parentName='mainPage'></OurWork>
     </div>
   );
 }
