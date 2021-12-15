d3c07799a2dd2a7e829b68ff3ec9b1f4c9b23ed3
TP1997 <thanhphamlike@gmail.com>
2021-12-14 20:58:19 -0500

    modify navBar and not finish the responsive layout-later

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,14 +1,92 @@
 import React from 'react';
-import Button from './Button';
-import User from './User';
+import { useHistory, useLocation } from 'react-router';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import './_navbar.scss'
 const Navbar = () => {
+  const { pathname } = useLocation();
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  const navBarHandleClick = () => {
+    document.querySelector(".navBar__list").classList.toggle("navBarListToggle");
+    document.querySelectorAll(".navBar__list li").forEach((li, index) => {
+      li.classList.toggle('liToggle');
+      if (li.style.animation) {
+        li.style.animationDelay = "";
+        console.log(li.style.animation);
+      } else {
+        li.style.animationDelay = `${index / 5 + 0.3}s`;
+      }
+      // console.log(li.style.animation);
+    })
+    console.log("run handle click");
+  }
+
+  const DynamicSignup = ({ loggedIn }) => {
+    const { pathname } = useLocation();
+    const history = useHistory();
+
+    const handleSignout = () => {
+      localStorage.clear();
+      history.push('/');
+    }
+
+
+    if (loggedIn) {
+      return (
+        <>
+          <li className={pathname === "/create" ? "active" : ""}>
+            <Link to="/create">New Blog</Link>
+          </li>
+          <li className={pathname === "/signout" ? "active" : ""} onClick={handleSignout}>
+            <Link to="/signout">Sign out</Link>
+          </li>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <li className={pathname === "/signup" ? "active" : ""}>
+            <Link to="/signup">Sign up</Link>
+          </li>
+          <li className={pathname === "/signin" ? "active" : ""}>
+            <Link to="/signin">Sign in</Link>
+          </li>
+        </>
+      )
+    }
+  }
+  // Add event for menu
+  // the bavBarHandleClick run many times?
+  useEffect(() => {
+    const navBarButton = document.querySelector('.navBar svg');
+    console.log(navBarButton.getAttribute('listener'));
+    if (navBarButton.getAttribute('listener')) navBarButton.removeEventListener("click", navBarHandleClick);
+    navBarButton.addEventListener("click", navBarHandleClick)
+    console.log('run effect');
+  }, [localStorage.getItem('userId') ? true : false])
+
+  // update the loggedIn status every time user log in or log out
+  useEffect(() => {
+    let test = localStorage.getItem('userId') ? true : false;
+    setLoggedIn(test);
+    console.log(isLoggedIn);
+  }, [localStorage.getItem('userId')])
+
   return (
     <div className='navBar'>
-      {/* <p>test font </p> */}
-      <Button class='navBar__home' text='HOME'/>
-      <Button class='navBar__topics' text='TOPICS'/>
-      <User class='navBar__user'/>
+      <Link className="navBar__brandname" to='/'>HVAC Blog</Link>
+      <ul className="navBar__list">
+        <li className="navBar__home"><Link to="/">Home</Link></li>
+        <li className="navBar__about"><Link to="/about">About</Link></li>
+        <li className="navBar__services"><Link to="/services">Services</Link></li>
+        <li className="navBar__blogs"><Link to="/blogs">Blogs</Link></li>
+        <li className="navBar__signin"><Link to="/signin">Sign In</Link></li>
+        <li className="navBar__signup"><Link to="/signup">Sign Up</Link></li>
+      </ul>
+      <FontAwesomeIcon icon={faBars} />
     </div>
   );
 }
