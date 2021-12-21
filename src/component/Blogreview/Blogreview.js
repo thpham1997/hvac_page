7836a2f04b6f9680a37b0cde9c10079e30b3d5fb
TP1997 <thanhphamlike@gmail.com>
2021-12-20 23:24:10 -0500

    style block review as card

--- src/component/Blogreview/Blogreview.js
+++ src/component/Blogreview/Blogreview.js
@@ -1,16 +1,18 @@
 import { faThumbsDown, faThumbsUp } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from 'react-router-dom'
+import './_blogreview.scss'
 import React from 'react'
-
+import pic3 from '../../img/headerImage.jpg'
 export default function Blogreview({ id, title, author, avatar, upvote, downvote }) {
 
   return (
-    <div className="blogreview" >
-      <img className="blogreview__img" height="50%" src={avatar} alt="" />
+    <div className="blogreview">
+      <img className="blogreview__img" /*height="50%"*/ src={avatar} alt="No Image" />
       <div className="blogreview__body">
         <h5 className="blogreview__title">{title}</h5>
         <p className="blogreview__text">Post created by {author.username}</p>
-        <div >
+        <div className="blogreview__control">
           <button onClick={() => { alert('View this blog to upvote it') }}>
             <FontAwesomeIcon icon={faThumbsUp} />
           </button> {upvote}
