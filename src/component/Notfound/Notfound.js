a00f26750388c0142148563e236475ad4c5df1af
TP1997 <thanhphamlike@gmail.com>
2021-12-31 21:55:21 -0500

    Add notfound page

--- src/component/Notfound/Notfound.js
+++ src/component/Notfound/Notfound.js
@@ -1,10 +1,23 @@
 import { faSadCry } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import { Link } from 'react-router-dom'
+import './_notfound.scss'
 export default function Notfound() {
+
+  function HeaderImg(props) {
+    return (
+      <div className={`${props.parentName}__headerImg`}>
+        <h2>WE ARE SORRY!</h2>
+      </div>
+    )
+  }
   return (
-    <div>
-      <FontAwesomeIcon icon={faSadCry}/>
+    <div className='notfoundPage'>
+      <HeaderImg parentName='notfoundPage' />
+      <div className='notfoundPage__body'>
+        <FontAwesomeIcon icon={faSadCry} />
+        <Link to="/">Go Back to Home Page</Link>
+      </div>
     </div>
   )
 }
