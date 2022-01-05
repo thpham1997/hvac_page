afa64a176770a15848f16af67663c1995b7eeb17
TP1997 <thanhphamlike@gmail.com>
2022-01-04 22:19:46 -0500

    Adding reposive design for mainPage-not done

--- src/component/Mainpage/Mainpage.js
+++ src/component/Mainpage/Mainpage.js
@@ -9,7 +9,6 @@ const Mainpage = () => {
   return (
     <div className='mainPage'>
       <Backgroundheader class='mainPage__bgImage' header='HVAC Group-Working for The Best'/>
-      {/* <Topicshortcut class='mainPage__topicShortcut'/> */}
       <Introduce parentName='mainPage'></Introduce>
       <OurWork parentName='mainPage'></OurWork>
       <Members parentName='mainPage'></Members>
